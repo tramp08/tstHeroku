d8fa24ab0a120d78bd360a7beda9f50e7edecb62
tramp08 <oleg@botov.org>
2021-04-04 19:47:02 +0300

    Alice-1 DEBUG os

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # импортируем библиотеки
+import os
 from flask import Flask, request
 import logging
 
@@ -132,4 +133,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
